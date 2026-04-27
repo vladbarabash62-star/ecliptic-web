953f4d8c600b29db5e0c67dcb87eed303686a46e
Vlad <vladbarabash62@gmail.com>
2026-04-28 00:40:50 +0300

    Fix import path for products

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { getProductBySlug, products } from "../../../lib/products";
+import { getProductBySlug, products } from "@/lib/products";
 
 type ProductPageProps = {
   params: Promise<{ slug: string }>;
